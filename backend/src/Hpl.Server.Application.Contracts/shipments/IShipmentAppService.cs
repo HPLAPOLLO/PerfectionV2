d99131f7856635174f9b82a4aff37ade34d05caf
Marcelo Ramos <mramos@hplapollo.com>
2022-11-16 15:25:46 -0500

    add Implement to Interface

---
+++ backend/src/Hpl.Server.Application.Contracts/shipments/IShipmentAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Hpl.Server.shipments
+{
+    public interface IShipmentAppService :
+        ICrudAppService<
+            ShipmentDto, 
+            Guid, 
+            PagedAndSortedResultRequestDto>
+    {
+    }
+}
---
+++ backend/src/Hpl.Server.Application/shipments/ShipmentAppService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Hpl.Server.shipments
+{
+    public class ShipmentAppService :
+        CrudAppService<
+            Shipment, 
+            ShipmentDto,
+            Guid,
+            PagedAndSortedResultRequestDto>, 
+        IShipmentAppService 
+    {
+        public ShipmentAppService(IRepository<Shipment, Guid> repository)
+            : base(repository)
+        {
+
+        }
+    }
+}
