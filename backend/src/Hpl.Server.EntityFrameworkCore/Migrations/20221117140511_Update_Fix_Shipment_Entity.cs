38b7abec15832105e280ddac40e78294a6528943
Marcelo Ramos <mramos@hplapollo.com>
2022-11-17 09:05:49 -0500

    Fix Migrator

---
+++ backend/src/Hpl.Server.EntityFrameworkCore/Migrations/20221117140511_Update_Fix_Shipment_Entity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hpl.Server.Migrations
+{
+    public partial class Update_Fix_Shipment_Entity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Flighgt",
+                table: "AppShipments",
+                newName: "Flight");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "JwAtd",
+                table: "AppShipments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Flight",
+                table: "AppShipments",
+                newName: "Flighgt");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JwAtd",
+                table: "AppShipments",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
