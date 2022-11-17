384d3a3850fd497e834dcb9c2aabaafa58b9961a
Marcelo Ramos <mramos@hplapollo.com>
2022-11-17 09:03:12 -0500

    Fix Type and word

--- backend/src/Hpl.Server.Domain/shipments/Shipment.cs
+++ backend/src/Hpl.Server.Domain/shipments/Shipment.cs
@@ -18,14 +18,14 @@ namespace Hpl.Server.shipments
         public string CarrierCode { get; set; }
         public string OhCode { get; set; }
         public int LegOrder { get; set; }
-        public string Flighgt { get; set; }
+        public string Flight { get; set; }
         public string LoadPort { get; set; }
         public DateTime Etd { get; set; }
         public DateTime Eta { get; set; }
         public DateTime Ata { get; set; }
         public int AtdCt { get; set; }
         public int AtaCt { get; set; }
-        public string JwAtd { get; set; }
+        public DateTime JwAtd { get; set; }
         public string DischargePort { get; set; }
         public bool IsLinked { get; set; }
     }
