12868ab1a69312a2e91e7766f81db078848f6760
Marcelo Ramos <mramos@hplapollo.com>
2022-11-18 14:19:06 -0500

    Backend: Added GetListAsync to Services

--- backend/src/Hpl.Server.Application.Contracts/shipments/IShipmentAppService.cs
+++ backend/src/Hpl.Server.Application.Contracts/shipments/IShipmentAppService.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
 namespace Hpl.Server.shipments
 {
-    public interface IShipmentAppService :
-        ICrudAppService<
-            ShipmentDto, 
-            Guid, 
-            PagedAndSortedResultRequestDto>
+    public interface IShipmentAppService : IApplicationService
     {
+        Task<List<ShipmentDto>> GetListAsync();
     }
 }
--- backend/src/Hpl.Server.Application/shipments/ShipmentAppService.cs
+++ backend/src/Hpl.Server.Application/shipments/ShipmentAppService.cs
@@ -1,26 +1,51 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
+using static OpenIddict.Abstractions.OpenIddictConstants;
 
 namespace Hpl.Server.shipments
 {
-    public class ShipmentAppService :
-        CrudAppService<
-            Shipment, 
-            ShipmentDto,
-            Guid,
-            PagedAndSortedResultRequestDto>, 
-        IShipmentAppService 
+    public class ShipmentAppService : ApplicationService, IShipmentAppService
     {
-        public ShipmentAppService(IRepository<Shipment, Guid> repository)
-            : base(repository)
-        {
+        private readonly IRepository<Shipment, Guid> _shipmentRepository;
 
+        public ShipmentAppService(IRepository<Shipment, Guid> shipmentRepository)
+        {
+            _shipmentRepository = shipmentRepository;
+        }
+
+        public async Task<List<ShipmentDto>> GetListAsync()
+        {
+            var items = await _shipmentRepository.GetListAsync();
+            return items
+                .Select(item => new ShipmentDto
+                {
+                    Branch = item.Branch,
+                    Origin = item.Origin,
+                    Destination = item.Destination,
+                    ShipmentId = item.ShipmentId,
+                    AirwayBillNumber = item.AirwayBillNumber,
+                    CarrierCode = item.CarrierCode,
+                    OhCode = item.OhCode,
+                    LegOrder = item.LegOrder,
+                    Flight = item.Flight,
+                    LoadPort = item.LoadPort,
+                    Etd = item.Etd,
+                    Eta = item.Eta,
+                    Ata = item.Ata,
+                    AtdCt = item.AtdCt,
+                    AtaCt = item.AtaCt,
+                    JwAtd = item.JwAtd,
+                    DischargePort = item.DischargePort,
+                    IsLinked = item.IsLinked
+                }).ToList();
         }
     }
 }
