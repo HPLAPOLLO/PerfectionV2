3905b48945c2d1a863519b7e8818a4e044ae456b
Marcelo Ramos <mramos@hplapollo.com>
2022-11-21 12:37:37 -0500

    Frontend: added rowexpansion

--- frontend/src/app/shipment/shipment.component.ts
+++ frontend/src/app/shipment/shipment.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ListService, PagedResultDto } from '@abp/ng.core';
 import { ShipmentDto, ShipmentService } from '@proxy/shipments';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-shipment',
   templateUrl: './shipment.component.html',
   styleUrls: ['./shipment.component.scss'],
-  providers: [ListService]
+  providers: [ListService],
+  animations: [
+    trigger('rowExpansionTrigger', [
+        state('void', style({
+            transform: 'translateX(-10%)',
+            opacity: 0
+        })),
+        state('active', style({
+            transform: 'translateX(0)',
+            opacity: 1
+        })),
+        transition('* <=> *', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)'))
+    ])
+]
 })
 export class ShipmentComponent implements OnInit {
 
   shipment: ShipmentDto[];
+  expansionShipment: ShipmentDto[];
 
   constructor(private shipmentService: ShipmentService) { }
 
@@ -18,8 +33,7 @@ export class ShipmentComponent implements OnInit {
 
     this.shipmentService.getList().subscribe((response) => {
       this.shipment = response;
-
-      console.log(response);
+      this.expansionShipment =  response;
     });
   }
 
